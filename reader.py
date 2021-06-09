31e409d01e17cb56d199e5d47398184154a8126d
Peter <40338605+LaptopKing@users.noreply.github.com>
2021-06-09 13:16:50 +0200

    Added HexToDecimal function
    This function will make sure that it converts grayscale data into colors (for now the only colors are between black and white) and then print the image out.

--- reader.py
+++ reader.py
@@ -1,17 +1,35 @@
 import numpy as np
 from time import sleep
 import ColorANSIRGB
+import sys
+
+image = ""
+
+try:
+    image = open(str(sys.argv[1]))
+except Exception as e:
+    print (e)
+    print ("[-] Failed running the program!\n[*] Probably there is no image what you have given or you did not give a full image name!")
+    exit()
 
 color = ColorANSIRGB
-
-X = 960
-Y = 960
 
 image = open("image.jpg")
 
 img = np.fromfile(image, dtype=np.uint8, count = X * Y)
 
-def findNum(img):
+def DecToHex(decimal):
+    conversion_table = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+    hexadecimal = ''
+
+    while (decimal > 0):
+        remainder = deciaml % 16
+        hexadecimal = conversion_table[remainder] + hexadecimal
+        decimal = decimal // 16
+    
+    return hexadecimal
+
+def FindNum(img):
     size = img.sizenums = []
     for i in range (size):
         try:
@@ -24,13 +42,14 @@ def findNum(img):
         nums.remove(max(nums))
 
     return max(nums)
-num = findNum(img)
+
+num = FindNum(img)
 
 img.shape = (img.size // num, num)
 
 for i in img:
     for k in i:
-        print (k, end=' ')
+        print (DecToHex(k), end=' ')
         sleep (0.75)
     sleep(1)
     print()
